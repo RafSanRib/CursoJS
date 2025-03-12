7b23734535e1b2bf66f9f88ffef312fa538106eb
Rafael S Ribeiro <rafasantosribeiro777@gmail.com>
2025-03-11 21:33:07 -0300

    Novas atualizações

---
+++ AULA 21/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igual a
+=== igual e do mesmo tipo
+!= diferente de
+!== diferente, inclusive do tipo
+
+Operadores lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+console comp = 10 > 5;   // true
+console.log(comp);
+
+  // true
