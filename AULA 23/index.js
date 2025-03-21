19515da30a7287169f4e3fe52d28742aff4aa769
Rafael S Ribeiro <rafasantosribeiro777@gmail.com>
2025-03-20 22:13:54 -0300

    Atualização

--- AULA 23/index.js
+++ AULA 23/index.js
@@ -3,4 +3,11 @@
 ||-> false || true -> true "o valor verdadeiro"
 
 */
-console.log(true && true);
+const a =0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e); //false
+console.log(a && b && c && d && e); //0
---
+++ AULA 24/index.js
@@ -0,0 +1,27 @@
+/*
+Entre 0 -11 - Bom dia
+Entre 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+
+*/
+/*
+const hora = 13;
+
+if (hora >= 0  && hora <=11){
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <=17) {
+    console.log('Boa tarde');
+} else if (hora >= 18 && hora <=23) {
+    console.log('Boa noite');
+} else {
+    console.log('Olá');
+}
+*/
+
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Não vou sair de casa');
+}
